9f0d2d95f4f6f6e06da3280b7bc93891bad887bd
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-27 10:46:45 -0300

    2.0

--- run.bat
+++ run.bat
@@ -1,12 +1,19 @@
 @echo off
 setlocal
 
-:: Define o caminho do Node.js portátil
-set NODE_DIR=%~dp0nodejs
-set PATH=%NODE_DIR%;%PATH%
+:: Verifica se o Node.js está instalado
+where node >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Node.js not found. Installing...
+    powershell -Command "& {Invoke-WebRequest -Uri 'https://nodejs.org/dist/v20.11.1/node-v20.11.1-x64.msi' -OutFile 'node_installer.msi'}"
+    msiexec /i node_installer.msi /norestart
+    echo Wait a moment...
+    timeout /t 15
+    echo Node.js installed with sucess!
+) else (
+    echo Node.js already installed.
+)
 
-:: Executa o script do Playwright
-cd /d "%~dp0projeto"
-%NODE_DIR%\node.exe script.js
-
+:: Executa o script Node.js
+node script.js
 pause
