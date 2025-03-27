a3b25f5bf05f2086e514f600d0eb2499fe251411
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-27 10:03:15 -0300

    adding bat

--- run.bat
+++ run.bat
@@ -1,14 +1,6 @@
 @echo off
 setlocal
 
-:: Eleva permissões automaticamente caso não tenha privilégios de administrador
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Solicitando permissões de administrador...
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
-    exit
-)
-
 :: Verifica se o Node.js está instalado
 where node >nul 2>nul
 if %errorlevel% neq 0 (
@@ -22,25 +14,16 @@ if %errorlevel% neq 0 (
     echo Node.js já está instalado.
 )
 
-:: Verifica se há um package.json na pasta atual
-if not exist package.json (
-    echo Nenhum package.json encontrado! Certifique-se de estar no diretório correto.
-    pause
-    exit /b
-)
-
-:: Verifica se os pacotes estão instalados
-echo Verificando pacotes do Node.js...
-npm ls --depth=0 >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Dependências não encontradas. Instalando...
+:: Verifica se a pasta app-retrieval/node_modules existe
+if not exist "app-retrieval\node_modules" (
+    echo Instalando dependências...
+    cd app-retrieval
     npm install
+    cd ..
 ) else (
-    echo Todos os pacotes já estão instalados.
+    echo Dependências já instaladas.
 )
 
 :: Executa o script Node.js
-echo Iniciando script.js...
 node script.js
-
 pause
